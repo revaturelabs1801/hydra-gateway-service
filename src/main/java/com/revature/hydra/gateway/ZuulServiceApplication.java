d0ee7ce166f8a62d7349daabc5f4b2ad5f0e4531
Saleh Shehata <Sal@USF-Gold-Wifi-226-110-227.laptops.usf.edu>
2018-03-20 12:35:28 -0400

    Remove Cors filter.

--- src/main/java/com/revature/hydra/gateway/ZuulServiceApplication.java
+++ src/main/java/com/revature/hydra/gateway/ZuulServiceApplication.java
@@ -29,23 +29,23 @@ public class ZuulServiceApplication {
 		SpringApplication.run(ZuulServiceApplication.class, args);
 	}
 	
-  @Bean
-  public CorsFilter corsFilter() {
-    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    final CorsConfiguration config = new CorsConfiguration();
-    config.setAllowCredentials(true);
-    config.addAllowedOrigin("*");
-    config.addAllowedHeader("*");
-    config.addAllowedMethod("OPTIONS");
-    config.addAllowedMethod("HEAD");
-    config.addAllowedMethod("GET");
-    config.addAllowedMethod("PUT");
-    config.addAllowedMethod("POST");
-    config.addAllowedMethod("DELETE");
-    config.addAllowedMethod("PATCH");
-    source.registerCorsConfiguration("/**", config);
-    return new CorsFilter(source);
-  }
+//  @Bean
+//  public CorsFilter corsFilter() {
+//    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    final CorsConfiguration config = new CorsConfiguration();
+//    config.setAllowCredentials(true);
+//    config.addAllowedOrigin("*");
+//    config.addAllowedHeader("*");
+//    config.addAllowedMethod("OPTIONS");
+//    config.addAllowedMethod("HEAD");
+//    config.addAllowedMethod("GET");
+//    config.addAllowedMethod("PUT");
+//    config.addAllowedMethod("POST");
+//    config.addAllowedMethod("DELETE");
+//    config.addAllowedMethod("PATCH");
+//    source.registerCorsConfiguration("/**", config);
+//    return new CorsFilter(source);
+//  }
 
 //	@Bean
 //    public Docket api() {
